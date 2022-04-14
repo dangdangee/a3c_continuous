014c3918cac94b52dfba0f44e6fbf50a30fb78fc
dangdangee <85330649+dangdangee@users.noreply.github.com>
2022-04-14 14:33:46 +0900

    minor revisionn

--- gym-custom/gym_custom/envs/custom_env.py
+++ gym-custom/gym_custom/envs/custom_env.py
@@ -7,8 +7,8 @@ import numpy as np
 class CustomEnv(gym.Env):
   metadata = {'render.modes': ['human']}
   def __init__(self):
-    self.init_state = np.zeros((700*5000,1),dtype=np.uint8)
-    self._state = np.zeros((700*5000,1),dtype=np.uint8)
+    self.init_state = np.zeros((70*500,1),dtype=np.uint8)
+    self._state = np.zeros((70*500,1),dtype=np.uint8)
     self._step = 0
     self.action_space = spaces.Box(
       # low=np.array([1, 1, 1, 1]), # x y w h
@@ -23,8 +23,8 @@ class CustomEnv(gym.Env):
       low=0,
       high=1,
       # shape=(700,5000,1),
-      shape = (700*5000,),
-      # 0.1 mm
+      shape = (70*500,),
+      # 1 mm
       dtype=np.uint8
     )
   def step(self, action):
@@ -32,20 +32,20 @@ class CustomEnv(gym.Env):
     yy = action[1]*399+1
     ww = action[2]*49+1
     hh = action[3]*99+1
-    x=np.around(xx*10)
-    y=np.around(yy*10)
-    w=np.around(ww*10)
-    h=np.around(hh*10)
-    temp_state = np.zeros((700,5000,1),dtype=np.uint8)
+    x=np.around(xx)
+    y=np.around(yy)
+    w=np.around(ww)
+    h=np.around(hh)
+    temp_state = np.zeros((70,500,1),dtype=np.uint8)
     temp_state[x:w,y:h] = 1
-    temp_state2 = temp_state.reshape((700*5000,1))
+    temp_state2 = temp_state.reshape((70*500,1))
     if np.sum(ob*temp_state2)==0:
       checker = True
     else:
       checker = False
 
     if checker==True:
-      ob = self._state + temp_state
+      ob = self._state + temp_state2
     else:
       ob = self._state
     rew = np.sum(ob)
@@ -59,7 +59,7 @@ class CustomEnv(gym.Env):
     return self.init_state
     # initial state
   def render(self, mode='human', close=False):
-    temp_state = self._state.reshape(700,5000)
+    temp_state = self._state.reshape(70,500)
     return temp_state
     # imageio.imread
 
